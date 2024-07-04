73146db0203e5ca9408faaa7dcbacfc1c8f39ef4
xiaoshihou514 <onelessononeexercise@gmail.com>
2024-07-04 19:27:23 +0800

    feat: many sorted logic support

--- src/main/scala/ndpc/syntax/Formula.scala
+++ src/main/scala/ndpc/syntax/Formula.scala
@@ -1,15 +1,15 @@
 package ndpc.syntax
 
 object Formula {
-    case class Function(name: String, arity: Int)
-    case class Predicate(name: String, arity: Int)
+    case class Function(name: String, arity: Int, sort: Vector[String])
+    case class Predicate(name: String, arity: Int, sort: Vector[String])
     // Definition 4.2 (term)
     // Fix a signature L.
     enum LTerm:
         // 1. Any constant in L is an L-term.
-        case Constant(name: String)
+        case Constant(name: String, sort: String)
         // 2. Any variable is an L-term.
-        case Variable(name: String)
+        case Variable(name: String, sort: String)
         // 3. If f is an n-ary function symbol in L, and t1...tn are L-terms, then f (t1...tn) is an L-term.
         case FuncAp(f: Function, xs: List[LTerm])
         // 4. Nothing else is an L-term.
@@ -44,11 +44,17 @@ object Formula {
             extends LFormula[Equiv[A, B]]
 
         // 5. If 𝝓 is an L-formula and x a variable, then (∀x 𝝓) and (∃x 𝝓) are L-formulas.
-        case Forall[A](vars: String, body: LFormula[A])
-            extends LFormula[Forall[A]]
+        case Forall[A](
+            vars: List[String],
+            sorts: List[String],
+            body: LFormula[A]
+        ) extends LFormula[Forall[A]]
 
-        case Exists[A](vars: String, body: LFormula[A])
-            extends LFormula[Exists[A]]
+        case Exists[A](
+            vars: List[String],
+            sorts: List[String],
+            body: LFormula[A]
+        ) extends LFormula[Exists[A]]
 
         // 6. Nothing else is an L-formula.
 }
