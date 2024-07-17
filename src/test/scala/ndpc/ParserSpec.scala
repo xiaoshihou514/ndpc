0d33e214331ef5db97d019a2a6a0e503f66f7ea5
xiaoshihou514 <onelessononeexercise@gmail.com>
2024-07-17 14:47:53 +0800

    fix: backtracking and unreachable match

--- src/test/scala/ndpc/ParserSpec.scala
+++ src/test/scala/ndpc/ParserSpec.scala
@@ -10,14 +10,30 @@ class ParserSpec extends UnitSpec {
         import parsley.syntax.character.{charLift, stringLift}
 
         sealed trait Pf
-        case class Stmt(body: String, ref: List[String]) extends Pf
-        case class Scope(var body: List[Pf]) extends Pf
+        case class Stmt(body: String, ref: List[String]) extends Pf {
+            override def toString() = s"$body(${ref.mkString(", ")})"
+        }
+        case class Scope(var body: List[Pf]) extends Pf {
+            def toStringWithIndent(x: Int): String = body
+                .map { pf =>
+                    pf match {
+                        case stmt @ Stmt(_, _) => " ".repeat(x * 4) + stmt
+                        case scope @ Scope(_) => scope.toStringWithIndent(x + 1)
+                    }
+                }
+                .mkString("\n")
+            override def toString() = toStringWithIndent(0)
+        }
 
         case class State(
-            level: Int,
+            var level: Int,
             lines: List[String],
-            scope: List[Scope]
-        )
+            var scope: List[Scope]
+        ) {
+            override def toString(): String =
+                s"level: $level\n" + s"lookup: $lines\nscope:\n" + scope
+                    .mkString("\n")
+        }
         val number = digit.foldLeft1[Int](0)((n, d) => n * 10 + d.asDigit)
 
         // format: off
@@ -39,30 +55,49 @@ class ParserSpec extends UnitSpec {
                     many(' ').map(_.length) <~> stmt <~ '\n'
                 ).map { res => (s: State) =>
                     val sc = s.scope
-                    val i = res._1
+                    val (i, sm) = res
                     if i == s.level then
-                        s.copy(scope = sc.head.copy(body = sc.head.body :+ res._2) :: sc.tail)
+                        sc.head.body = sc.head.body :+ sm
                     else if i == s.level + 4 then
-                        val newScope = Scope(List(res._2))
+                        // new scope
+                        val newScope = Scope(List(sm))
                         sc.head.body = sc.head.body :+ newScope
-                        s.copy(
-                            level = s.level + 4,
-                            scope = newScope :: sc
-                        )
+                        s.level += 4
+                        s.scope = newScope :: s.scope
                     else if i == s.level - 4 then
                         // prev scope
                         val t = sc.tail
-                        t.head.body = t.head.body :+ res._2
-                        s.copy(
-                            level = s.level - 4,
-                            scope = t
-                        )
+                        t.head.body = t.head.body :+ sm
+                        s.level -= 4
+                        s.scope = t
                     else
                         fail()  // TODO: better errors
+                    s
                 }
             )) ~> state.get.map { s => s.scope.last }
             scope
         }
-        println(p.parse("boo()\nbar(1)\n    baz(1, 2)\nfizz()\nbuzz(4)\n").get)
+        // format: on
+        val testStr = """
+boo()
+bar(1)
+    baz(1, 2)
+        fizz()
+        buzz()
+    h(1)
+g(2)
+n(3)
+            """.trim() + "\n"
+        val output = """
+boo()
+bar(boo)
+    baz(boo, bar)
+        fizz()
+        buzz()
+    h(boo)
+g(bar)
+n(baz)
+            """.trim()
+        assert(p.parse(testStr).get.toString() === output)
     }
 }
