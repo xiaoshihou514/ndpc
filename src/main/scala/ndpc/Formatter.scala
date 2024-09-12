7defa622cc1ffc9c1382e91eed7146b92c199022
charbs <charbs.sunny@gmail.com>
2024-09-12 21:31:13 +0800

    ref: removed generics according to changes in checker

--- src/main/scala/ndpc/Formatter.scala
+++ src/main/scala/ndpc/Formatter.scala
@@ -8,19 +8,19 @@ object Formatter {
 
     private def maxOf(target1 : Int, target2: Int) : Int = if (target2 > target1) target2 else target1
 
-    def findReasonAlign(target : PfScope[LF_], ini_indent : Int = 0) : Int = {
+    def findReasonAlign(target : PfScope, ini_indent : Int = 0) : Int = {
         var result = 0
-        target.body.foreach((line)=> if (line.isInstanceOf[PfScope[LF_]]){
+        target.body.foreach((line)=> if (line.isInstanceOf[PfScope]){
                 //go deeper if we see another block
-                result = maxOf(result, findReasonAlign(line.asInstanceOf[PfScope[LF_]], ini_indent = ini_indent + 1))
-            }else if (line.isInstanceOf[Pf[?]]){
+                result = maxOf(result, findReasonAlign(line.asInstanceOf[PfScope], ini_indent = ini_indent + 1))
+            }else if (line.isInstanceOf[Pf]){
                 // update result if we see a new useful line
-                result = maxOf(result, (line.asInstanceOf[Pf[?]]).concl.toString().length())
+                result = maxOf(result, (line.asInstanceOf[Pf]).concl.toString().length())
             })
         return result + ini_indent * 2
     }
 
-    private def lineParser(line : Line[_], indent : Int, reasonAlign : Int) : String = {
+    private def lineParser(line : Line, indent : Int, reasonAlign : Int) : String = {
         line match{
             case Empty() => return ""
             case Comment(contents) => return " " * indent * 2 + contents
@@ -30,14 +30,14 @@ object Formatter {
         }
     }
 
-    private def scopeFormatter(target : PfScope[LF_], currentIndent : Int, reasonAlign : Int) : String = {
+    private def scopeFormatter(target : PfScope, currentIndent : Int, reasonAlign : Int) : String = {
         var result = ""
         target.body.foreach((line)=>
             if (result != "") result + "\n"
-            if (line.isInstanceOf[PfScope[?]]){
-                result + scopeFormatter(line.asInstanceOf[PfScope[LF_]], currentIndent + 1, reasonAlign)
+            if (line.isInstanceOf[PfScope]){
+                result + scopeFormatter(line.asInstanceOf[PfScope], currentIndent + 1, reasonAlign)
             }else{
-                result + lineParser(line.asInstanceOf[Line[?]], currentIndent, reasonAlign)
+                result + lineParser(line.asInstanceOf[Line], currentIndent, reasonAlign)
             })
         return result
     }
