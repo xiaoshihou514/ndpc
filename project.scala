035e581855db302799f2ba3ae4190c93dce6dc42
xiaoshihou <xiaoshihou@tutamail.com>
2025-01-05 18:28:30 +0000

    ref: remove java directory ritual

--- project.scala
+++ project.scala
@@ -2,5 +2,5 @@
 //> using platform native
 //> using nativeVersion 0.4.17
 //> using dep com.github.j-mie6::parsley::4.5.2
-//> using dep org.scalatest::scalatest::3.2.18
 //> using dep com.lihaoyi::os-lib::0.9.3
+//> using test.dep org.scalatest::scalatest::3.2.18
