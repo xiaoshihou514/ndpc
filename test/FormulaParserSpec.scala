d19a6cce67d7c03481fb845acf30dc3f8832bb9e
xiaoshihou <xiaoshihou@tutamail.com>
2025-01-05 18:33:40 +0000

    ref: use scalatest matchers

--- test/FormulaParserSpec.scala
+++ test/FormulaParserSpec.scala
@@ -10,8 +10,8 @@ given Conversion[String, PredAp] with
 class FormulaParserSpec extends UnitSpec {
     "A predAp" should "be a predicate applied to multiple lterms" in {
         val example1 = PredAp("foo", List("x", "y", "z"))
-        assert(predAp.parse("foo ( x, y, z )").get == example1)
-        assert(lformula.parse("foo ( x, y, z )").get == example1)
+        predAp.parse("foo ( x, y, z )").get shouldBe example1
+        lformula.parse("foo ( x, y, z )").get shouldBe example1
         val example2 = PredAp(
           "foo",
           List(
@@ -19,8 +19,8 @@ class FormulaParserSpec extends UnitSpec {
             "w"
           )
         )
-        assert(predAp.parse("foo( bar(ss,l), w)").get == example2)
-        assert(lformula.parse("foo( bar(ss,l), w)").get == example2)
+        predAp.parse("foo( bar(ss,l), w)").get shouldBe example2
+        lformula.parse("foo( bar(ss,l), w)").get shouldBe example2
         val example3 = PredAp(
           "foo",
           List(
@@ -34,24 +34,16 @@ class FormulaParserSpec extends UnitSpec {
             "w"
           )
         )
-        assert(
-          predAp
-              .parse("foo( bar(ss,wacc   (w  , a , c,c)  ), w)")
-              .get == example3
-        )
-        assert(
-          lformula
-              .parse("foo( bar(ss,wacc   (w  , a , c,c)  ), w)")
-              .get == example3
-        )
+        predAp.parse("foo( bar(ss,wacc   (w  , a , c,c)  ), w)").get shouldBe example3
+        lformula.parse("foo( bar(ss,wacc   (w  , a , c,c)  ), w)").get shouldBe example3
         val sugar = PredAp("foo", Nil)
-        assert(predAp.parse("foo^bar").get == sugar)
+        predAp.parse("foo^bar").get shouldBe sugar
     }
 
     "eq" should "be an predAp = an predAp" in {
         val example1 = Eq("a", "bb")
-        assert(equ.parse("a   = bb").get == example1)
-        assert(lformula.parse("a   = bb").get == example1)
+        equ.parse("a   = bb").get shouldBe example1
+        lformula.parse("a   = bb").get shouldBe example1
         val example2 = Eq(
           "x",
           PredAp(
@@ -59,8 +51,8 @@ class FormulaParserSpec extends UnitSpec {
             List("a", "wa")
           )
         )
-        assert(equ.parse("x=  wuu (  a, wa)").get == example2)
-        assert(lformula.parse("x=  wuu (  a, wa)").get == example2)
+        equ.parse("x=  wuu (  a, wa)").get shouldBe example2
+        lformula.parse("x=  wuu (  a, wa)").get shouldBe example2
         val example3 = Eq(
           PredAp("jkjk", Nil),
           PredAp(
@@ -70,13 +62,14 @@ class FormulaParserSpec extends UnitSpec {
             )
           )
         )
-        assert(equ.parse("jkjk ()= u(qo(j,w))").get == example3)
-        assert(lformula.parse("jkjk ()= u(qo(j,w))").get == example3)
+        equ.parse("jkjk ()= u(qo(j,w))").get shouldBe example3
+        lformula.parse("jkjk ()= u(qo(j,w))").get shouldBe example3
     }
 
     "Truth and falsity" should "be a single char T/F" in {
-        assert(truth.parse("T ^ fff").get == Truth())
-        assert(truth.parse("T").get == Truth())
+        truth.parse("T ^ fff").get shouldBe Truth()
+        truth.parse("T").get shouldBe Truth()
+
         assert(truth.parse("TasVar").isFailure)
         assert(falsity.parse("Fstart").isFailure)
     }
@@ -93,9 +86,7 @@ class FormulaParserSpec extends UnitSpec {
             "j"
           )
         )
-        assert(
-          atom.parse("TStartFunc (FStartFunc(a,b)  ,   fs(), j)").get == atom1
-        )
+        atom.parse("TStartFunc (FStartFunc(a,b)  ,   fs(), j)").get shouldBe atom1
     }
 
     "An lformula" should "be connectives + lfromula / forall(lformula) / exists(lformula)" in {
@@ -110,71 +101,43 @@ class FormulaParserSpec extends UnitSpec {
             "j"
           )
         )
-        assert(
-          lformula
-              .parse("(  TStartFunc (FStartFunc(a,b)  ,   fs(), j))")
-              .get == atomWithBrackets
-        )
+        lformula
+            .parse("(  TStartFunc (FStartFunc(a,b)  ,   fs(), j))")
+            .get shouldBe atomWithBrackets
 
         val connectives_1 = Not(atomWithBrackets)
-        assert(
-          lformula
-              .parse("~ (  TStartFunc (FStartFunc(a,b)  ,   fs(), j))")
-              .get == connectives_1
-        )
-        assert(
-          lformula
-              .parse("~(  TStartFunc (FStartFunc(a,b)  ,   fs(), j))")
-              .get == connectives_1
-        )
+        lformula.parse("~ (  TStartFunc (FStartFunc(a,b)  ,   fs(), j))").get shouldBe connectives_1
+        lformula.parse("~(  TStartFunc (FStartFunc(a,b)  ,   fs(), j))").get shouldBe connectives_1
 
         val connectives_2 = And(
           atomWithBrackets,
           Truth()
         )
-        assert(
-          lformula
-              .parse("TStartFunc (FStartFunc(a,b)  ,   fs(), j) ^T")
-              .get == connectives_2
-        )
-        assert(
-          lformula
-              .parse("TStartFunc (FStartFunc(a,b)  ,   fs(), j) ^  T")
-              .get == connectives_2
-        )
-        assert(
-          lformula
-              .parse("TStartFunc (FStartFunc(a,b)  ,   fs(), j)^  T")
-              .get == connectives_2
-        )
+        lformula.parse("TStartFunc (FStartFunc(a,b)  ,   fs(), j) ^T").get shouldBe connectives_2
+        lformula.parse("TStartFunc (FStartFunc(a,b)  ,   fs(), j) ^  T").get shouldBe connectives_2
+        lformula.parse("TStartFunc (FStartFunc(a,b)  ,   fs(), j)^  T").get shouldBe connectives_2
 
         val connectives_3 = Or(
           Falsity(),
           atomWithBrackets
         )
-        assert(
-          lformula
-              .parse("F/TStartFunc (FStartFunc(a,b)  ,   fs(), j)")
-              .get == connectives_3
-        )
+        lformula.parse("F/TStartFunc (FStartFunc(a,b)  ,   fs(), j)").get shouldBe connectives_3
 
         val connectives_4 = Implies(
           atomWithBrackets,
           atomWithBrackets
         )
-        assert(
-          lformula
-              .parse(
-                "(TStartFunc(FStartFunc(a,b),fs(),j)) ->  TStartFunc (FStartFunc(a,b)  ,   fs(), j)"
-              )
-              .get == connectives_4
-        )
+        lformula
+            .parse(
+              "(TStartFunc(FStartFunc(a,b),fs(),j)) ->  TStartFunc (FStartFunc(a,b)  ,   fs(), j)"
+            )
+            .get shouldBe connectives_4
 
         val connectives_5 = Equiv(
           Truth(),
           Falsity()
         )
-        assert(lformula.parse("(((((T)))))    <->  (F)").get == connectives_5)
+        lformula.parse("(((((T)))))    <->  (F)").get shouldBe connectives_5
 
         val connectives_6 = And(
           Implies(
@@ -186,7 +149,7 @@ class FormulaParserSpec extends UnitSpec {
             PredAp("r", Nil)
           )
         )
-        assert(lformula.parse("(p -> q) ^ (~p -> r)").get == connectives_6)
+        lformula.parse("(p -> q) ^ (~p -> r)").get shouldBe connectives_6
 
         val connectives_7 = And(
           Eq("p", "q"),
@@ -195,19 +158,13 @@ class FormulaParserSpec extends UnitSpec {
             PredAp("r", Nil)
           )
         )
-        assert(
-          lformula.parse("(p=    q   )^    ( ~  p-> r  )").get == connectives_7
-        )
+        lformula.parse("(p=    q   )^    ( ~  p-> r  )").get shouldBe connectives_7
 
         val forall = Forall(
           "q",
           connectives_7
         )
-        assert(
-          lformula
-              .parse("forall q. ((p=    q   )^    ( ~  p-> r  ))")
-              .get == forall
-        )
+        lformula.parse("forall q. ((p=    q   )^    ( ~  p-> r  ))").get shouldBe forall
 
         val exists = Forall(
           "𝝓",
@@ -228,13 +185,9 @@ class FormulaParserSpec extends UnitSpec {
             )
           )
         )
-        assert(
-          lformula
-              .parse(
-                "forall 𝝓 . (exists A. ( foo(𝝓 ,φ ,A) / (exists B. (bar(𝝓, B)))))"
-              )
-              .get == exists
-        )
+        lformula
+            .parse("forall 𝝓 . (exists A. ( foo(𝝓 ,φ ,A) / (exists B. (bar(𝝓, B)))))")
+            .get shouldBe exists
 
         val precedence = Implies(
           And(
@@ -246,7 +199,7 @@ class FormulaParserSpec extends UnitSpec {
             PredAp("a", Nil)
           )
         )
-        assert(lformula.parse("a^b->(a<->a)").get == precedence)
+        lformula.parse("a^b->(a<->a)").get shouldBe precedence
 
     }
 
@@ -261,11 +214,9 @@ class FormulaParserSpec extends UnitSpec {
             PredAp("fly", List("x"))
           )
         )
-        assert(
-          lformula
-              .parse("forall x. (dragon(x) ^ green(x) -> fly(x))")
-              .get == `all green dragons can fly`
-        )
+        lformula
+            .parse("forall x. (dragon(x) ^ green(x) -> fly(x))")
+            .get shouldBe `all green dragons can fly`
     }
 
     "An LFormula" should "respect braces" in {
@@ -277,8 +228,6 @@ class FormulaParserSpec extends UnitSpec {
                 PredAp("b", Nil)
               )
             )
-        assert(
-          lformula.parse("a ^(a ->b)").get == precedence
-        )
+        lformula.parse("a ^(a ->b)").get shouldBe precedence
     }
 }
